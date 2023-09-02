ca4be67b4be54e770ac760bbd8e7cc6e03e1da9a
adonaiviedondon <vieiraadonai@gmail.com>
2023-09-02 16:53:37 -0300

    conceitos

--- exerciciobruno/src/ConceitosBasicos/Fahreinheitparacelsius.java
+++ exerciciobruno/src/ConceitosBasicos/Fahreinheitparacelsius.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 public class Fahreinheitparacelsius {
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner (System.in);
-		System.out.print("digite a temperatura em fahreinheit");
+		System.out.print("digite a temperatura em fahreinheit:");
 		double temp = leitor.nextDouble();
+		double celsius =(5*(temp-32)/9);
+		System.out.printf("temperatura em celsius é %.2f",(celsius));
 	}
 
 }
