331dd83a1adaf443bd9e5b269120ba2d0ab19f2c
adonaiviedondon <vieiraadonai@gmail.com>
2023-09-07 18:48:37 -0300

    ingresso

--- exerciciobruno/src/EstruturadeDecisao/Ingresso.java
+++ exerciciobruno/src/EstruturadeDecisao/Ingresso.java
@@ -3,71 +3,66 @@ package EstruturadeDecisao;
 import java.util.Scanner;
 
 public class Ingresso {
-	public static void main(String[] args) {
-		Scanner leitor = new Scanner(System.in);
-		System.out.print("digite um numero do dia da semana");
-		int diadasemana = leitor.nextInt();
-		System.out.print("digite a idade do telespectador");
-		int idade = leitor.nextInt();
-		System.out.print("digite o valor do ingresso");
-		double ingresso = leitor.nextDouble();
-		if (diadasemana < 1 || diadasemana > 7) {
-			System.out.print("digite um numero do dia da semana");
-			int diadasemana1 = leitor.nextInt();
-		}
-		if (1 <= diadasemana && diadasemana <= 7) {
+    public static void main(String[] args) {
+        int diadasemana = 0;
+        int idade = 0;
+        double ingresso = 0.0;
 
-			switch (diadasemana) {
-			case 1:
-				System.out.println("idade:" + idade);
-				System.out.println("valor a pagar:" + ingresso);
-				System.out.println("dia da semana:Domingo");
-				break;
-			case 2:
-				System.out.println("idade:" + idade);
-				System.out.println("valor a pagar:" + ingresso);
-				System.out.println("dia da semana:Segunda");
-				break;
-				
-			case 3:
-				if (idade < 14) {
-					System.out.println("idade:" + idade);
-					System.out.println("valor a pagar:" + (ingresso/2));
-					System.out.println("dia da semana:Terça");
-					break;
-				}
-				else {
-					System.out.println("idade:" + idade);
-					System.out.println("valor a pagar:" + (ingresso));
-					System.out.println("dia da semana:Terça");
-					break;
-				}
-			case 4:
-				System.out.println("idade:" + idade);
-				System.out.println("valor a pagar:" + ingresso);
-				System.out.println("dia da semana:Quarta");
-				break;
-			case 5:
-				System.out.println("idade:" + idade);
-				System.out.println("valor a pagar:" + ingresso);
-				System.out.println("dia da semana:Quinta");
-				break;
-			case 6:
-				System.out.println("idade:" + idade);
-				System.out.println("valor a pagar:" + ingresso);
-				System.out.println("dia da semana:Sexta");
-				break;
-			case 7:
-				System.out.println("idade:" + idade);
-				System.out.println("valor a pagar:" + ingresso);
-				System.out.println("dia da semana:Sabado");
-				break;
-				}
-	     else {
-				
-			}
-			}
-		}
-	
+        Scanner leitor = new Scanner(System.in);
+        while (diadasemana < 1 || diadasemana > 7) {
+            System.out.print("Digite um número do dia da semana : ");
+            diadasemana = leitor.nextInt();
 
-	}
+            if (diadasemana < 1 || diadasemana > 7) {
+                System.out.println("numeroinvalido");
+            }
+        }
+
+        System.out.print("Digite a idade do telespectador: ");
+        idade = leitor.nextInt();
+        System.out.print("Digite o valor do ingresso: ");
+        ingresso = leitor.nextDouble();
+
+        switch (diadasemana) {
+            case 1:
+                System.out.println("Dia da semana: Domingo");
+                System.out.println("Idade: " + idade);
+                System.out.println("Valor a pagar: " + ingresso);
+                break;
+            case 2:
+                System.out.println("Dia da semana: Segunda");
+                System.out.println("Idade: " + idade);
+                System.out.println("Valor a pagar: " + ingresso);
+                break;
+            case 3:
+                System.out.println("Dia da semana: Terça");
+                if (idade < 14) {
+                	System.out.println("Idade: " + idade);
+                	System.out.println("Valor a pagar: " + ingresso/2);
+                }
+                break;
+            case 4:
+                System.out.println("Dia da semana: Quarta");
+                System.out.println("Idade: " + idade);
+            	System.out.println("Valor a pagar: " + ingresso);
+                break;
+            case 5:
+                System.out.println("Dia da semana: Quinta");
+                System.out.println("Idade: " + idade);
+            	System.out.println("Valor a pagar: " + ingresso);
+                break;
+            case 6:
+                System.out.println("Dia da semana: Sexta");
+                System.out.println("Idade: " + idade);
+            	System.out.println("Valor a pagar: " + ingresso);
+                break;
+            case 7:
+                System.out.println("Dia da semana: Sábado");
+                System.out.println("Idade: " + idade);
+            	System.out.println("Valor a pagar: " + ingresso);
+                break;
+        }
+
+        
+    }
+}
